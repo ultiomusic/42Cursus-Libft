f0f9cda762ab910e122d049c85757c846ca0564f
Ultio <94832543+ultiomusic@users.noreply.github.com>
2023-01-20 01:00:39 +0300

    Add files via upload

---
+++ libft/ft_isalpha.c
@@ -0,0 +1,4 @@
+int ft_isalpha(int str)
+{
+    return((str >= 'A' && str <= 'Z') || (str >= 'z' && str <= 'z'));
+}
---
+++ libft/ft_isdigit.c
@@ -0,0 +1,4 @@
+int ft_isdigit(int str)
+{
+    return(str >= '0' && str <= '9');
+}
