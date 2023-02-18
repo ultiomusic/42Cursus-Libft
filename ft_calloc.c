d6721a549438d17572555fac8f47dd90d2ebf6b1
Ultio <94832543+ultiomusic@users.noreply.github.com>
2023-02-18 20:52:50 +0300

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -16,14 +16,11 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*result;
 
-	if (count * size < count)
-		return (NULL);
-	result = malloc(size * count);
+	result = malloc(count * size);
 	if (!result)
-		return (0);
-	ft_bzero(result, size * count);
+		return (NULL);
+	ft_bzero(result, count * size);
 	return (result);
-}
 /*
 int main()
 {
